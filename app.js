14c6232e11d45d01580babb17d30f111906bbede
brenda1018 <brenduh6656@gmail.com>
2025-04-14 20:48:56 -0700

    april12

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const msg = document.getElementById("confirmationMsg");
   
     form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      msg.style.display = "block"; // Show confirmation message
+        const firstName = document.getElementById("fname").value;
+        const lastName = document.getElementById("lname").value;
+        const question = document.getElementById("subject").value;
+        const email = document.getElementById("email").value;
+
+        if (!firstName || !lastName || !question || !email) {
+            e.preventDefault();
+            msg.style.display = "block";
+            msg.style.color = "red";
+            msg.textContent = "Fields incomplete!"
+        }
+        else {
+            e.preventDefault();
+            msg.style.display = "block";
+            msg.style.color = "green";
+            msg.textContent = "Form Submitted";
+            form.reset();
+        }
     });
   });
   
