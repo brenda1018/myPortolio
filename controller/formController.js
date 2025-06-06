c930c59b80c44f994a42a7e9141d00f385259efb
brenda1018 <brenduh6656@gmail.com>
2025-06-05 18:29:36 -0700

    updates css

--- controller/formController.js
+++ controller/formController.js
@@ -39,9 +39,13 @@ export async function handleFormSubmit(req, res) {
 // schema example 
 // add this data to the DB 
 // update query, check if email in the db and then update the comment, 
+  // create global class, mention functions inside it, then add export statement with class name
+  // create a single export
 // create a class container and you can add as many functions
+// make lists for summary for rojects and leadership 
 // single export -- one time, will ex port whole class
-// import the import class
+
+// for query create udpate function, check if emailid is in data base, if so update the FN LN AND comments. 
 // formController(class name).formSubmisson() 
 // formsubmission.handleform
-// make navbar bigger or format better
+// once db is done, move to middleware 
